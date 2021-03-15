9181e7053b1fc67e88e6253ddf2ffb0e791baa23
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-03-15 18:53:08 +0000

    feat: initial files copied across
    Now ready to start coding

---
+++ Performance Assignment Skeleton/Performance2/Performance2.cpp
@@ -0,0 +1,152 @@
+// Visual Studio 2017 version.
+
+#include "stdafx.h"
+#include "Performance2.h"
+
+#include <iostream>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// Timer - used to established precise timings for code.
+class TIMER
+{
+	LARGE_INTEGER t_;
+
+	__int64 current_time_;
+
+	public:
+		TIMER()	// Default constructor. Initialises this timer with the current value of the hi-res CPU timer.
+		{
+			QueryPerformanceCounter(&t_);
+			current_time_ = t_.QuadPart;
+		}
+
+		TIMER(const TIMER &ct)	// Copy constructor.
+		{
+			current_time_ = ct.current_time_;
+		}
+
+		TIMER& operator=(const TIMER &ct)	// Copy assignment.
+		{
+			current_time_ = ct.current_time_;
+			return *this;
+		}
+
+		TIMER& operator=(const __int64 &n)	// Overloaded copy assignment.
+		{
+			current_time_ = n;
+			return *this;
+		}
+
+		~TIMER() {}		// Destructor.
+
+		static __int64 get_frequency()
+		{
+			LARGE_INTEGER frequency;
+			QueryPerformanceFrequency(&frequency); 
+			return frequency.QuadPart;
+		}
+
+		__int64 get_time() const
+		{
+			return current_time_;
+		}
+
+		void get_current_time()
+		{
+			QueryPerformanceCounter(&t_);
+			current_time_ = t_.QuadPart;
+		}
+
+		inline bool operator==(const TIMER &ct) const
+		{
+			return current_time_ == ct.current_time_;
+		}
+
+		inline bool operator!=(const TIMER &ct) const
+		{
+			return current_time_ != ct.current_time_;
+		}
+
+		__int64 operator-(const TIMER &ct) const		// Subtract a TIMER from this one - return the result.
+		{
+			return current_time_ - ct.current_time_;
+		}
+
+		inline bool operator>(const TIMER &ct) const
+		{
+			return current_time_ > ct.current_time_;
+		}
+
+		inline bool operator<(const TIMER &ct) const
+		{
+			return current_time_ < ct.current_time_;
+		}
+
+		inline bool operator<=(const TIMER &ct) const
+		{
+			return current_time_ <= ct.current_time_;
+		}
+
+		inline bool operator>=(const TIMER &ct) const
+		{
+			return current_time_ >= ct.current_time_;
+		}
+};
+
+CWinApp theApp;  // The one and only application object
+
+using namespace std;
+
+
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	// initialize Microsoft Foundation Classes, and print an error if failure
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+		nRetCode = 1;
+	}
+	else
+	{
+		// Application starts here...
+
+		// Time the application's execution time.
+		TIMER start;	// DO NOT CHANGE THIS LINE. Timing will start here.
+
+		//--------------------------------------------------------------------------------------
+		// Insert your code from here...
+
+
+
+
+		//-------------------------------------------------------------------------------------------------------
+		// How long did it take?...   DO NOT CHANGE FROM HERE...
+		
+		TIMER end;
+
+		TIMER elapsed;
+		
+		elapsed = end - start;
+
+		__int64 ticks_per_second = start.get_frequency();
+
+		// Display the resulting time...
+
+		double elapsed_seconds = (double)elapsed.get_time() / (double)ticks_per_second;
+
+		cout << "Elapsed time (seconds): " << elapsed_seconds;
+		cout << endl;
+		cout << "Press a key to continue" << endl;
+
+		char c;
+		cin >> c;
+	}
+
+	return nRetCode;
+}
