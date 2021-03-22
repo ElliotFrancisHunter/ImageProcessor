4b04354b6dd9c66695cc0c43b3d9a66f5c8bdc3a
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-03-22 22:47:43 +0000

    feat: convert images to PNG format and save them
    Some redundant variables here. Send code to Paul for review. Enquire about how to use parallelism here.

--- Performance Assignment Skeleton/Performance2/ImageManager.cpp
+++ Performance Assignment Skeleton/Performance2/ImageManager.cpp
@@ -20,3 +20,30 @@ void ImageManager::ExtractFiles()
 		mImages.push_back(Gdiplus::Image::FromFile(file.path().filename().wstring().c_str()));
 	}
 }
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="images"></param>
+void ImageManager::SaveAsPNG(const std::vector<Gdiplus::Image*>& images)
+{
+	Gdiplus::GdiplusStartupInput input;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &input, NULL);
+
+	CLSID encoderClsid;
+	Gdiplus::Status stat;
+	int result;
+	WCHAR strGuid[39];
+
+	GetEncoderClsid(L"image/png", &encoderClsid);
+
+	int imgCount = 1;
+
+	for (const auto& image : images)
+	{
+		std::wstring convFileName = std::wstring(imgCount + L".png");;
+		image->Save(convFileName.c_str(), &encoderClsid, NULL);
+		++imgCount;
+	}
+}
