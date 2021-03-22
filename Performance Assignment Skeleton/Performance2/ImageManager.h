c98ba4a8bf2f397556c2b8c643ba7d9bb672aeb5
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-03-22 22:38:36 +0000

    feat: header implementation of methods
    Currently loading, saving and conversion and rotation are implemented.

--- Performance Assignment Skeleton/Performance2/ImageManager.h
+++ Performance Assignment Skeleton/Performance2/ImageManager.h
@@ -3,22 +3,38 @@
 #include <gdipluseffects.h>
 #include <filesystem>
 
-// Perhaps have a struct that contains all the item info such as the dimension etc
+// Perhaps have a struct that contains all the item info such as the dimension etc - EH
+
 
 class ImageManager
 {
 private:
-	// Needs a vector of strings for the file names
-	// Needs a vector of Images that is populated from said file names
-protected:
 
+	const std::string mSourcePath = "/Source/";
+	const std::string mDestPath = "/Destination/";
+
+	std::vector<std::string> mFileNames;
+	std::vector<Gdiplus::Image*> mImages; // Contains image objects derived from filenames - EH
+
+protected:
+	ImageManager();
 public:
 	// Needs a method to get all files from a folder
-	// Needs a method to create image objects(Can use the Image::FromFile method)
+	void ExtractFiles();
 	// Needs a method to convert jpgs to pngs(Use the Image::Save method, perhaps create a method of keeping the same file name but changing extension to png.
+	void SaveAsPNG(const std::vector<Gdiplus::Image*>& images);
 	// With Image::Save you can literally write a new PNG image by typing imageName.png and it'll do it for you
 	// Rotate method using Image::RotateFlip
+	void RotateImage(std::vector<Gdiplus::Image*> images);
 	// For Greyscale you can create a Gdiplus::ColorMatrix and apply the matrix to Gdiplus::ImageAttributes
-	// To adjust brightness, add a constant to the rgb fields within each image ensuring they don't go below zero or above the maximum bit depth(Once again, gdi plus has a BrightnessContrast Class
+	void ApplyGreyScale(std::vector<Gdiplus::Image*> images);
+	// To adjust brightness, add a constant to the rgb fields within each image 
+	// ensuring they don't go below zero or above the maximum bit depth
+	// (Once again, gdi plus has a BrightnessContrast Class
+	void BrightenImage(std::vector<Gdiplus::Image*> images);
+
+	void LinearScaleImage(std::vector<Gdiplus::Image*> images);
+
+	int GetEncoderClsid(const WCHAR* format, CLSID* pClsid);
 };
 
