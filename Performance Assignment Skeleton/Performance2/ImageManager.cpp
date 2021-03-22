0a195c4f0f5da1ae216830df25dc2b80918ca05d
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-03-22 22:48:48 +0000

    feat: create helper function for image encoding
    Perhaps ask Paul for help explaining this. For the most part it seems efficient but there may be stuff that can be trimmed.

--- Performance Assignment Skeleton/Performance2/ImageManager.cpp
+++ Performance Assignment Skeleton/Performance2/ImageManager.cpp
@@ -47,3 +47,47 @@ void ImageManager::SaveAsPNG(const std::vector<Gdiplus::Image*>& images)
 		++imgCount;
 	}
 }
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="format"></param>
+/// <param name="pClsid"></param>
+/// <returns></returns>
+int ImageManager::GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
+{
+	UINT  encoderCount = 0;          // number of image encoders
+	UINT  encoderArrSize = 0;         // size of the image encoder array in bytes
+
+	Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
+
+	Gdiplus::GetImageEncodersSize(&encoderCount, &encoderArrSize);
+
+	if (encoderArrSize == 0)
+	{
+		return -1;  // Failure
+	}
+
+	pImageCodecInfo = (Gdiplus::ImageCodecInfo*)(malloc(encoderArrSize));
+
+	if (pImageCodecInfo == NULL)
+	{
+		return -1;  // Failure
+	}
+
+	GetImageEncoders(encoderCount, encoderArrSize, pImageCodecInfo);
+
+	for (UINT j = 0; j < encoderCount; ++j)
+	{
+		if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0)
+		{
+			*pClsid = pImageCodecInfo[j].Clsid;
+			free(pImageCodecInfo);
+			return j;  // Success
+		}
+	}
+
+	free(pImageCodecInfo);
+	return -1;  // Failure
+}
