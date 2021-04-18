4d7c61f3e999f96a2e46e4345e09ab9a9097dc01
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-04-18 20:29:46 +0100

    Refactor: abandon gdi+ in favour of openCV
    MUCH faster and easier to work with

--- Performance Assignment Skeleton/Performance2/Performance2.cpp
+++ Performance Assignment Skeleton/Performance2/Performance2.cpp
@@ -2,12 +2,18 @@
 
 #include "stdafx.h"
 #include "Performance2.h"
-
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc.hpp"
 #include <iostream>
+#include <concurrent_vector.h>
+#include <ppl.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
+
+const std::string mSourcePath = "./Source/";
+const std::string mDestPath = "./Destination/";
 
 // Timer - used to established precise timings for code.
 class TIMER
@@ -100,10 +106,14 @@ CWinApp theApp;  // The one and only application object
 
 //using namespace std;
 
-
+//ImageManager im;
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
+	Gdiplus::GdiplusStartupInput input;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &input, NULL);
+
 	int nRetCode = 0;
 
 	// initialize Microsoft Foundation Classes, and print an error if failure
@@ -119,10 +129,22 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 		// Time the application's execution time.
 		TIMER start;	// DO NOT CHANGE THIS LINE. Timing will start here.
 
+		Concurrency::concurrent_vector<cv::Mat> matV;
 		//--------------------------------------------------------------------------------------
 		// Insert your code from here...
+		for (const auto& file : std::filesystem::directory_iterator(mSourcePath))
+		{
 
+		}
+		cv::Mat sourceMat, destMat;
+		sourceMat = cv::imread("./Source/IMG_1.jpg", cv::ImreadModes::IMREAD_GRAYSCALE);
+		cv::transpose(sourceMat, destMat); 
+		cv::flip(destMat, destMat, 1);
+		destMat.convertTo(destMat, -1, 1, 50); // Increase all pixel values by 50 to increase brightness
+		
+		cv::resize(destMat, destMat, cv::Size(destMat.rows * 2, destMat.cols * 2), 2, 2, cv::InterpolationFlags::INTER_LINEAR);
 
+		imwrite("Test2.png", destMat);
 
 
 		//-------------------------------------------------------------------------------------------------------
@@ -139,6 +161,8 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 		// Display the resulting time...
 
 		double elapsed_seconds = (double)elapsed.get_time() / (double)ticks_per_second;
+
+		Gdiplus::GdiplusShutdown(gdiplusToken);
 
 		std::cout << "Elapsed time (seconds): " << elapsed_seconds;
 		std::cout << std::endl;
